0b096a72c1321d4dd8801975282bb18b0567b713
U-Awesome\Diana <Diana@Awesome.(none)>
2011-11-14 18:13:15 -0600

    init clusters

--- kmeans/kmeans.clustering.R
+++ kmeans/kmeans.clustering.R
@@ -3,7 +3,10 @@
 #m <- kmeans.clustering("C:/cygwin/home/Diana/Clustering/Data/docword.kos.txt", 5, "Col1", "Col2")
 #m
 
-kmeans.clustering <- function(D, k, col1, col2) #D = Path to data file, k=number of clusters, col1 = Column name 1, col2 = Column name 2
+
+
+#D = Path to data file, k=number of clusters, col1 = Column name 1, col2 = Column name 2
+kmeans.clustering <- function(D, k, col1, col2) 
 {
 
 m <- read.Data(D, col1, col2)
@@ -13,7 +16,9 @@ return(m)
 
 
 
-read.Data <- function(Data, col1, col2) #Data = Location of txt file, col1 = Name of column1, col2 = Name of column2
+
+#Data = Location of txt file, col1 = Name of column1, col2 = Name of column2
+read.Data <- function(Data, col1, col2) 
 {
 
 m <- matrix(scan(Data), ncol=2, byrow = TRUE)
@@ -23,18 +28,53 @@ return(m)
 
 }
 
-init.Clusters <- function(Data, k) #Data = Matrix, k = number of clusters, partition data into k (random if you want) clusters. No return value.
+
+
+
+#Data = Matrix, k = number of clusters, partition data into k (random if you want) clusters. No return value.
+init.Clusters <- function(Data, k) 
 {
+
+
+for(i in 1:nrow(Data)){
+random <- sample(1:k, 1)
+Data[i, 3] <- random
 }
 
-calculate.centroid <- function(Data, id) #Calculate the centroid given the data matrix and the id of cluster. Return centroid coordinates.
-{
+return(Data)
 }
 
-cluster <- function(Data) #Use calculate.centroid to get centroid, and then assign every row to the closest cluster centroid. Return false if no row was assigned to a different cluster, true otherwise.
+
+
+#Calculate the centroid given the data matrix and the id of cluster. Return centroid coordinates.
+calculate.centroid <- function(Data, id) 
 {
+
+
+
+
 }
 
-draw.Clusters <- function(Data) #Draws the graph. Don't know much about this yet.
+
+
+#Use calculate.centroid to get centroid, and then assign every row to the closest cluster centroid. Return false if no row was assigned to a different cluster, true otherwise.
+cluster <- function(Data) 
 {
+
+
+
+
+}
+
+
+
+#Draws the graph. Don't know much about this yet.
+draw.Clusters <- function(Data) 
+{
+
+
+
+
+
+
 }
